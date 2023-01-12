34dc4f1a82d6781f061ba969902298d747b7f6eb
Tomas Ferreras <hellotomasdev@gmail.com>
2023-01-11 22:52:37 -0300

    initial commit

---
+++ src/setupServer.ts
@@ -0,0 +1,66 @@
+import {
+  Application,
+  json,
+  urlencoded,
+  Response,
+  Request,
+  NextFunction,
+} from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
+import compression from 'compression';
+
+export class UnidevServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['test1', 'test2'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
+
+  private routesMiddleware(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
+  private startServer(app: Application): void {}
+
+  private startHttpServer(httpServer: http.Server): void {}
+  private createSocketIO(httpServer: http.Server): void {}
+}
